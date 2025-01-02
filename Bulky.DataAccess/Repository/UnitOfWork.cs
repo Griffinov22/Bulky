9382e919f34d445cb32301be00e5085004a3ecab
Griffin Overmyer <griffinovermyer@gmail.com>
2025-01-01 20:25:30 -0500

    add shopping cart to structure

--- Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -14,6 +14,8 @@ namespace Bulky.DataAccess.Repository
         public IProductRepository Product { get; private set; }
         public ICompanyRepository Company { get; private set; }
 
+        public IShoppingCartRepository ShoppingCart { get; private set; }
+
         private ApplicationDbContext _db;
         public UnitOfWork(ApplicationDbContext db)
         {
@@ -21,6 +23,7 @@ namespace Bulky.DataAccess.Repository
             Category = new CategoryRepository(_db);
             Product = new ProductRepository(_db);
             Company = new CompanyRepository(_db);
+            ShoppingCart = new ShoppingCartRepository(_db);
         }
 
         public void Save()
---
+++ Bulky.Models/ShoppingCart.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.Models
+{
+    // This is a 1 to many relationship of application users to shopping carts
+    public class ShoppingCart
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        [ValidateNever]
+        public Product Product { get; set; }
+        [Range(1,250, ErrorMessage = "Please enter value between 1 - 250")]
+        public int Count { get; set; }
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        [ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; }
+
+    }
+}
